c022a1a7e63980fc70837907e298267e8cb35cc5
hala-samir <h.samir2011@gmail.com>
2024-07-21 14:29:25 +0300

    handle pom version and run in docker image

--- src/test/java/stepdefinitions/TestBase.java
+++ src/test/java/stepdefinitions/TestBase.java
@@ -4,21 +4,30 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class TestBase extends AbstractTestNGCucumberTests{
     public static WebDriver driver;
+    static final String app_URL = "https://www.demoblaze.com";
+    static final String host_URL = "http://localhost:4444/wd/hub";
 
     @BeforeSuite
-    public void setup(){
+    public void setup() throws MalformedURLException {
         String browser = System.getProperty("browser", "chrome");
         if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().clearDriverCache().setup();
-            driver = new ChromeDriver();
+//            WebDriverManager.chromedriver().clearDriverCache().setup();
+//            driver = new ChromeDriver();
+            ChromeOptions options=new ChromeOptions();
+            driver= new RemoteWebDriver(new URL(host_URL), options);
         } else if (browser.equalsIgnoreCase("edge"))
         {
             WebDriverManager.edgedriver();
@@ -31,11 +40,12 @@ public class TestBase extends AbstractTestNGCucumberTests{
             driver = new SafariDriver();
     }
         driver.manage().window().maximize();
-       driver.get("https://www.demoblaze.com");
+       driver.get(app_URL);
     }
 
     @AfterSuite
     public void teardown(){
-        driver.quit();
+        if(driver != null)
+            driver.quit();
     }
 }
